bebc633b4de374678da2482e08e29c4b4a28871c
Nikita Marchant <nikita.marchant@gmail.com>
2021-01-09 22:58:32 +0100

    Change setup.py to read the markdown readme

--- setup.py
+++ setup.py
@@ -10,35 +10,31 @@ with open("alabaster_hotwire/_version.py") as fp:
 version = _locals["__version__"]
 
 # README into long description
-with codecs.open("README.rst", encoding="utf-8") as f:
+with codecs.open("README.md", encoding="utf-8") as f:
     readme = f.read()
 
 setup(
     name="alabaster_hotwire",
     version=version,
-    description="A configurable sidebar-enabled Sphinx theme",
+    description="A fork of Alabaster to match the Hotwire.dev style",
     long_description=readme,
     author="Jeff Forcier",
     author_email="jeff@bitprophet.org",
-    url="https://alabaster.readthedocs.io",
+    url="https://github.com/hotwire-django/sphinx-hotwire-theme",
     packages=["alabaster_hotwire"],
     include_package_data=True,
     entry_points={"sphinx.html_themes": ["alabaster_hotwire = alabaster_hotwire"]},
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Documentation",
